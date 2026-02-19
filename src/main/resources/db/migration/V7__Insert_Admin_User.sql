7fb91df5b4a7e8ec53f268caf70b9d51094c485e
nitin <nitin.manitcse@gmail.com>
2026-02-19 07:33:22 +0530

    implement redis,kafka,ratel imiter

--- src/main/resources/db/migration/V7__Insert_Admin_User.sql
+++ src/main/resources/db/migration/V7__Insert_Admin_User.sql
@@ -2,7 +2,7 @@
 INSERT INTO users (username, password, name, email, role, usage_limit, analysis_count, generation_count, created_at, updated_at)
 VALUES (
     'admin', 
-    '$2a$10$8.UnVuG9HHgffUDAlk8qfOuVGkqRzgVymGe07xd00DMxs.TVuHOn2', -- password: admin123
+    '$2a$10$OTLAVfGvPgE/lbJOHShxK.LatpNWdZCAEJsEz8CD.lWBxn3YozM6a',
     'Administrator', 
     'admin@example.com', 
     'ADMIN', 
--- src/main/resources/db/migration/V8__Update_Admin_Password.sql
+++
@@ -1,4 +0,0 @@
--- Update Admin User Password to 'admin123'
-UPDATE users 
-SET password = '$2a$10$OTLAVfGvPgE/lbJOHShxK.LatpNWdZCAEJsEz8CD.lWBxn3YozM6a' 
-WHERE username = 'admin';
